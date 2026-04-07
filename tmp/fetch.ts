81036ae15e20dd918ff5234730f057a643bfc769
nath78osb <151663455+nath78osb@users.noreply.github.com>
2026-04-07 17:42:38 +0100

    Create fetch.ts

---
+++ tmp/fetch.ts
@@ -0,0 +1,14 @@
+import https from 'https';
+
+https.get('https://www.labellerr.com/blog/cricket-ball-detection/', (res) => {
+  let data = '';
+  res.on('data', (chunk) => { data += chunk; });
+  res.on('end', () => {
+    const match = data.match(/<article[^>]*>([\s\S]*?)<\/article>/i);
+    if (match) {
+        console.log(match[1].replace(/<[^>]+>/g, ' ').substring(0, 3000));
+    } else {
+        console.log("No article found");
+    }
+  });
+});
